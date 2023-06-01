b85240c2fe332fc5005cb16fe4a04b9b99443e81
Joel Tondozi <joeltondozi@gmail.com>
2023-06-01 11:41:27 +0100

    feat: create hero section on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,16 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
          <Layout>
-       <h1 >Accueil</h1>
-       </Layout>
+          <section className="flex flex-col w-full  h-[80vh] bg-[url('/agrobibi/hero.png')] bg-center bg-no-repeat bg-cover" >
+            <div class="w-full h-full flex flex-col  justify-center items-center backdrop-brightness-50">
+                  <span className="flex flex-col w-2/3 text-center border-t border-b-2 p-3 pt-8 pb-8" >
+                    <h1 className='text-white text-4xl font-bold pb-4'>La technologie pour soutenir l’agriculutre !</h1> 
+                    <p className='text-white text-lg font-semibold'>Inscrivez-vous pour commencer à recevoir des conseils et des informations en temps réel par sms sur les bonnes partiques agricoles </p>
+                  </span>
+                  <button className=' text-center mt-8 w-1/4 bg-white  p-3 rounded-full text-[#189643] font-bold text'>Inscrivez-vous</button>
+            </div>
+          </section>
+         </Layout>
   
      
   )
