32c0e10018714d44b6fdc9a704f866af3333e810
Joel Tondozi <joeltondozi@gmail.com>
2023-06-02 13:09:54 +0100

    fix prettier config

--- src/components/ProductContainer.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-function ProductContainer() {
-  return <div className="flex w-full flex-col space-x-2"></div>;
-}
-
-export default ProductContainer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,60 +1,78 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import Layout from '@/components/layout'
-import SearchBar from '@/components/SearchBar'
-import CategoriesContainer from '@/components/CategoriesContainer'
+import Image from 'next/image';
 
+import Layout from '@/components/layout';
 
-const inter = Inter({ subsets: ['latin'] })
+import CategoriesContainer from '@/components/CategoriesContainer';
 
 export default function Home() {
   return (
-         <Layout>
-          <section className="flex flex-col w-full  h-[80vh] bg-[url('/agrobibi/hero.png')] bg-center bg-no-repeat bg-cover" >
-            <div class="w-full h-full flex flex-col  justify-center items-center backdrop-brightness-50">
-                  <span className="flex flex-col w-2/3 text-center border-t border-b-2 p-3 pt-8 pb-8 mb-10" >
-                    <h1 className='text-white text-4xl font-bold pb-4'>La technologie pour soutenir l’agriculutre !</h1> 
-                    <p className='text-white text-lg font-semibold'>Inscrivez-vous pour commencer à recevoir des conseils et des informations en temps réel par sms sur les bonnes partiques agricoles </p>
-                  </span>
-                  <button className=' text-center mt-8 w-1/4 bg-white  p-3 rounded-full text-[#189643] font-bold text'>Inscrivez-vous</button>
-            </div>
-          </section>
+    <Layout>
+      <section className="flex h-[80vh] w-full  flex-col bg-[url('/agrobibi/hero.png')] bg-cover bg-center bg-no-repeat">
+        <div className="flex h-full w-full flex-col  items-center justify-center backdrop-brightness-50">
+          <span className="mb-10 flex w-2/3 flex-col border-b-2 border-t p-3 pb-8 pt-8 text-center">
+            <h1 className="pb-4 text-4xl font-bold text-white">
+              La technologie pour soutenir l’agriculutre !
+            </h1>
+            <p className="text-lg font-semibold text-white">
+              Inscrivez-vous pour commencer à recevoir des conseils et des
+              informations en temps réel par sms sur les bonnes partiques
+              agricoles{' '}
+            </p>
+          </span>
+          <button className=" text mt-8 w-1/4 rounded-full  bg-white p-3 text-center font-bold text-[#189643]">
+            Inscrivez-vous
+          </button>
+        </div>
+      </section>
 
-          <section className='flex flex-row w-full h-[80vh] justify-between  pl-8 pr-8 items-center' >
-            <div className='flex flex-row w-full justify-between'>
-              <div>
-                <h1 className='text-[#189643] font-bold text-4xl pb-3 pt-2'>Ce que nous faisons </h1>
-                <div className='bg-[#189643] w-1/4 h-[5px] rounded-full'></div>
-                <p className='w-2/3 pt-8 pb-8'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
-Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley  
-              </p>
-               <button className='border-solid border border-[#189643] text-[#189643] p-2  pr-4 pl-4 rounded-full' > connexion</button>
-              </div>
-              
-              <Image src="/agrobibi/sect.png" width={800} height={447} className='w-[680px] h-[447px]' />
-            </div>
-            
-          </section>
-           <section className=' h-[100vh] '>
-            <div className="flex flex-col w-full  h-[45vh] bg-[url('/agrobibi/Legumes.png')] bg-center bg-no-repeat bg-cover" >
-            <div class="w-full h-full flex flex-col  justify-center items-center backdrop-brightness-50">
-                  <span className="flex flex-col w-2/3 text-center border-t border-b-2 p-3 pt-8 pb-8 mb-10" >
-                    <h1 className='text-white text-4xl font-bold pb-4'>Commandez les meilleurs produits bio et frais!</h1> 
-                    <p className='text-white text-lg font-semibold'>Et faites vous livrer à domicile le jour de votre choix </p>
-                  </span> 
-            </div>
+      <section className="flex h-[80vh] w-full flex-row items-center  justify-between pl-8 pr-8">
+        <div className="flex w-full flex-row justify-between">
+          <div>
+            <h1 className="pb-3 pt-2 text-4xl font-bold text-[#189643]">
+              Ce que nous faisons{' '}
+            </h1>
+            <div className="h-[5px] w-1/4 rounded-full bg-[#189643]"></div>
+            <p className="w-2/3 pb-8 pt-8">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industrys standard dummy text
+              ever since the 1500s, when an unknown printer took a galley Lorem
+              Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry s standard dummy text
+              ever since the 1500s, when an unknown printer took a galley
+            </p>
+            <button className="rounded-full border border-solid border-[#189643] p-2  pl-4 pr-4 text-[#189643]">
+              {' '}
+              connexion
+            </button>
           </div>
-           <div className='pl-8 pr-8'>
-            <CategoriesContainer/>
-           </div>
-          
-          </section>
-           
 
+          <Image
+            src="/agrobibi/sect.png"
+            width={800}
+            height={447}
+            className="h-[447px] w-[680px]"
+          />
+        </div>
+      </section>
 
-         </Layout>
-  
-     
-  )
+      <section className=" h-[100vh] ">
+        <div className="flex h-[45vh] w-full  flex-col bg-[url('/agrobibi/Legumes.png')] bg-cover bg-center bg-no-repeat">
+          <div className="flex h-full w-full flex-col  items-center justify-center backdrop-brightness-50">
+            <span className="mb-10 flex w-2/3 flex-col border-b-2 border-t p-3 pb-8 pt-8 text-center">
+              <h1 className="pb-4 text-4xl font-bold text-white">
+                Commandez les meilleurs produits bio et frais!
+              </h1>
+              <p className="text-lg font-semibold text-white">
+                Et faites vous livrer à domicile le jour de votre choix{' '}
+              </p>
+            </span>
+          </div>
+        </div>
+
+        <div className="pl-8 pr-8">
+          <CategoriesContainer />
+        </div>
+      </section>
+    </Layout>
+  );
 }
