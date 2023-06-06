d9dacf1f83fc8644fde1da40867ab1230bb927c3
Joel Tondozi <joeltondozi@gmail.com>
2023-06-06 14:40:10 +0100

    feat : add section text

--- src/pages/index.js
+++ src/pages/index.js
@@ -40,13 +40,14 @@ export default function Home() {
               height={447}
               className="w-[680px] pt-8  md:hidden"
             />
-            <p className="w-full pb-8  pt-8 md:w-2/3">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley
+            <p className="w-full pb-8  pt-8 text-lg md:w-2/3">
+              Nous sommes une startup agroalimentaire qui produit et transforme
+              le cacao en poudre, le chia et autres épices. Nous nous appuyons
+              sur la technologie sur la technologie pour partager les
+              informations nécessaires aux agriculteurs en République
+              Démocratique du Congo. Notre plateforme en ligne qui lui sert de
+              canal de diffusion d’informations destinées aux agriculteurs que
+              nous encadrons à Kinshasa, capitale de la RDC.
             </p>
             <button className="rounded-full border border-solid border-[#189643] p-2  pl-4 pr-4 text-[#189643]">
               {' '}
@@ -105,19 +106,20 @@ export default function Home() {
               height={447}
               className="flex h-[auto] w-1/2 pt-8 md:hidden"
             />
-            <p className="w-full pt-8 md:pb-8">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley
+            <p className="w-full pt-8 text-lg md:pb-8">
+              Le partenariat avec Agrobibi est un excellent moyen pour les
+              agriculteurs de la République Démocratique du Congo de
+              s&apos;associer à une entreprise innovante et novatrice, qui met à
+              leur disposition des technologies de pointe. En vous associant à
+              DRC Agro, vous aurez accès à des informations, des conseils et des
+              outils qui vous aideront à améliorer vos méthodes de production et
+              à maximiser vos profits. Nous sommes fiers de notre capacité à
+              offrir des produits et des services que les agriculteurs ne
+              trouvent pas dans d&apos;autres entreprises. Avec l&apos;aide de
+              notre plateforme en ligne, nous sommes en mesure de vous fournir
+              des informations précises et à jour, des technologies novatrices
+              &apos;et des conseils d&apos;experts en matière de production
+              alimentaire
             </p>
             <button className="rounded-full border border-solid border-[#189643] p-2  pl-4 pr-4 text-[#189643]">
               {' '}
@@ -134,13 +136,18 @@ export default function Home() {
               Formations & Conférences{' '}
             </h1>
             <div className="h-[5px] w-1/4 rounded-full bg-[#189643]"></div>
-            <p className="w-3/4 pb-8 pt-8">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industrys standard dummy text
-              ever since the 1500s, when an unknown printer took a galley Lorem
-              Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry s standard dummy text
-              ever since the 1500s, when an unknown printer took a galley
+            <p className="w-3/4 pb-8 pt-8 text-lg">
+              Nos formations et conférences sont conçues pour fournir des
+              informations sur les dernières technologies et outils agricoles,
+              les stratégies concurrentielles et les bonnes pratiques agricoles.
+              Nous abordons également des sujets tels que l’amélioration des
+              méthodes de production et de transformation, l’utilisation des
+              technologies avancées et la résolution des problèmes auxquels les
+              agriculteurs font face. Nous sommes fiers de participer à
+              l’amélioration des conditions des agriculteurs et de contribuer à
+              la production de produits alimentaires sûrs et de qualité. Notre
+              équipe est qualifiée et expérimentée dans tous les domaines
+              agricoles.
             </p>
             <button className="rounded-full border border-solid border-[#189643] p-2  pl-4 pr-4 text-[#189643]">
               {' '}
